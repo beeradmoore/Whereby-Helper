9599b7cdc3fec8daf987c62811c656e40cd4dd57
Brad Moore <brad@4pi.com.au>
2020-12-30 16:00:19 +1100

    Added ability to focus chat. Added notifications for when extension enables/disables camera/mic.

--- Browser Extension/background.js
+++ Browser Extension/background.js
@@ -2,7 +2,7 @@ chrome.commands.onCommand.addListener(function(command) {
     //console.log('Command:', command);
     
     // First lets see if its a valid command.
-    if ((command == "toggle-cam" || command == "toggle-mic") == false)
+    if ((command == "toggle-cam" || command == "toggle-mic" || command == "focus-tab") == false)
     {
         return;
     }
@@ -10,11 +10,57 @@ chrome.commands.onCommand.addListener(function(command) {
     // Now lets find all whereby tabs.
     chrome.tabs.query({ "url": "https://*.whereby.com/*" }, function(tabs) {
         //console.log("Found " + tabs.length + " tabs");
-        for (var i = 0; i < tabs.length; i++)
+
+        if (tabs.length == 1)
         {
-            // And letes send the action to it.
-            //console.log(tabs[i].id + " - " + tabs[i].url);
-            chrome.tabs.sendMessage(tabs[i].id, { action: command });
+            var tab = tabs[0];
+
+            if (command == "focus-tab")
+            {
+                chrome.tabs.update(tab.id, { "active" : true });
+                chrome.windows.update(tab.windowId, {"focused": true}, null);
+            }
+            else
+            {
+                //for (var i = 0; i < tabs.length; i++)
+                //{
+                    // And lets send the action to it.
+                    //console.log(tabs[i].id + " - " + tabs[i].url);
+                    chrome.tabs.sendMessage(tab.id, { action: command }, (response) => {
+                        // Did it do the thing?
+
+                        // Something bad happened, no notification for you.
+                        if (response == undefined)
+                        {
+                            return;
+                        }
+
+                        var message = "";
+                        if (command == "toggle-cam")
+                        {
+                            message = "Camera is now ";
+                        }
+                        else if (command == "toggle-mic")
+                        {
+                            message = "Microphone is now ";
+                        }
+
+                        if (response.new_state === true)
+                        {
+                            message += "on.";
+                        }
+                        else if (response.new_state === false)
+                        {
+                            message += "off.";
+                        }
+                        chrome.notifications.create(null, { iconUrl: "Icon_48.png", message: message, title: "Whereby Helper", type: "basic" });
+                    });
+                //}
+            }
+        }
+        else
+        {
+            chrome.notifications.create(null, { iconUrl: "Icon_48.png", message: "Multiple WhereBy tabs detected. Doing nothing 🤷‍♂️", title: "Whereby Helper", type: "basic" });
         }
     });
 });
--- Browser Extension/content.js
+++ Browser Extension/content.js
@@ -10,6 +10,13 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         if (camButton != undefined)
         {
             camButton.click();
+
+            // If the new state is not undefined lets send it back to background for notification.
+            var isCameraOn = IsCameraOn();
+            if (isCameraOn === true || isCameraOn === false)
+            {
+                sendResponse({ new_state: isCameraOn});
+            }
         }
     }
     else if (message.action == "toggle-mic")
@@ -18,6 +25,100 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         if (micButton != undefined)
         {
             micButton.click();
+
+            // If the new state is not undefined lets send it back to background for notification.
+            var isMicOn = IsMicrophoneOn();
+
+            if (isMicOn === true || isMicOn === false)
+            {
+                sendResponse({ new_state: isMicOn});
+            }
         }
     }
 });
+
+function IsCameraOn()
+{
+    // Unsure if this can be compressed. Just trying to make sure it doesn't break extension.
+    var camButton = $(".jstest-toggle-video");
+
+    //console.log("camButton");
+    //console.log(camButton);
+
+    if (camButton == undefined || $(camButton).children().length == 0)
+    {
+        return;
+    }
+
+    var figureButton = $(camButton).children()[0];
+
+    //console.log("figureButton");
+    //console.log(figureButton);
+
+    if (figureButton == undefined || $(figureButton).children().length == 0)
+    {
+        return;
+    }
+
+    var buttonIconWrapper = $(figureButton).children()[0];
+
+    if (buttonIconWrapper == undefined)
+    {
+        return;
+    }
+
+    var classList = buttonIconWrapper.classList;
+    for (var i = 0; i < classList.length; ++i)
+    {
+        var className = classList[i];
+        if (className.startsWith("isOff"))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function IsMicrophoneOn()
+{
+    // Unsure if this can be compressed. Just trying to make sure it doesn't break extension.
+    var micButton = $(".jstest-mute-button");
+
+    //console.log("micButton");
+    //console.log(micButton);
+
+    if (micButton == undefined || $(micButton).children().length == 0)
+    {
+        return;
+    }
+
+    var figureButton = $(micButton).children()[0];
+
+    //console.log("figureButton");
+    //console.log(figureButton);
+
+    if (figureButton == undefined || $(figureButton).children().length == 0)
+    {
+        return;
+    }
+
+    var buttonIconWrapper = $(figureButton).children()[0];
+
+    if (buttonIconWrapper == undefined)
+    {
+        return;
+    }
+
+    var classList = buttonIconWrapper.classList;
+    for (var i = 0; i < classList.length; ++i)
+    {
+        var className = classList[i];
+        if (className.startsWith("isOff"))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
